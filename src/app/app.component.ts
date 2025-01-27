ce5364380b847f1de9632efd0aeb2e91b1ca0e62
Yazar <yazararafath1612@gmail.com>
2025-01-27 23:46:42 +0530

    sidemenu issue fixed by yaz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import {SideMenuComponent} from './shared/side-menu/side-menu.component';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule, FormsModule, SideMenuComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet,RouterModule, FormsModule, SideMenuComponent, HeaderComponent, FooterComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Skillhive Learning Nexus';
+
+  isMenuVisible: boolean = false;
+
+  onMenuToggle() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
 }
